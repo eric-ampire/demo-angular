4e7e2b594214e7e9a01e024a40e0c6f2603c4bce
Eric Ampire <ericampire.top@gmail.com>
2019-11-25 16:48:47 +0200

    Adding Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
